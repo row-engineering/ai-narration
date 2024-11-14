c087054129911240b7296aaa3eae85d106bb7e5b
Anna Rasshivkina <anna.nigma@yahoo.com>
2024-11-14 12:30:25 -0500

    support query param for generate.php

--- endpoint/generate.php
+++ endpoint/generate.php
@@ -7,11 +7,16 @@ if (!defined('BASE_PATH')) {
 }
 require BASE_PATH . '/wp-load.php';
 
-$post_id = 71145;	// http://restofworld.test/2024/venezuela-online-gig-work/
+$post_id = $_GET['p'];
+
+if (!$post_id) {
+	return;
+}
+
 $post = get_post($post_id);
 
 $plugin = new AI_Narration();
 $plugin_public = new AI_Narration_Public( $plugin->get_plugin_name(), $plugin->get_version() );
 
 $response = $plugin_public->request_new_audio('publish', 'draft', $post);
-// error_log(json_encode($response));
+error_log(json_encode($response));
