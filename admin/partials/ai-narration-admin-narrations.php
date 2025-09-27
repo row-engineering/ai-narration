5f3ca3ad2a5c42c54f02f748f37af7ce34a8922c
Michael Donohoe <donohoe@gmail.com>
2025-09-27 11:24:44 -0400

    Switch from buttons to WP usual Bulk Edit UI

--- admin/partials/ai-narration-admin-narrations.php
+++ admin/partials/ai-narration-admin-narrations.php
@@ -19,10 +19,24 @@ $table->prepare_items();
 ?>
 
 <div class="wrap" id="ain-page-narrations">
-	<div class="tablenav top">
-		<div class="alignleft actions bulkactions">
+	<div class="tablenav top hide-if-no-js">
+		<!-- <div class="alignleft actions bulkactions">
 			<button type="button" id="bulk-generate" class="button action">Generate Selected</button>
 			<button type="button" id="bulk-delete" class="button action">Delete Selected</button>
+		</div> -->
+
+		<div class="alignleft actions bulkactions">
+			<label for="bulk-action-selector" class="screen-reader-text">Select bulk action</label>
+			<select name="action" id="bulk-action-selector">
+				<option value="-1">Bulk actions</option>
+				<option value="generate">Generate</option>
+				<option value="delete">Delete</option>
+			</select>
+			<button type="button" id="bulk-apply" class="button action">Apply</button>
+		</div>
+
+		<div class="alignright">
+			<span id="ain-status">&nbsp;</span>
 		</div>
 	</div>
 
