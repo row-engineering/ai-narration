580d9e09988257b38d33d95c96b5eec77addf34e
Michael Donohoe <donohoe@gmail.com>
2025-09-27 13:17:47 -0400

    Remove unused markup

--- admin/partials/ai-narration-admin-narrations.php
+++ admin/partials/ai-narration-admin-narrations.php
@@ -20,11 +20,6 @@ $table->prepare_items();
 
 <div class="wrap" id="ain-page-narrations">
 	<div class="tablenav top hide-if-no-js">
-		<!-- <div class="alignleft actions bulkactions">
-			<button type="button" id="bulk-generate" class="button action">Generate Selected</button>
-			<button type="button" id="bulk-delete" class="button action">Delete Selected</button>
-		</div> -->
-
 		<div class="alignleft actions bulkactions">
 			<label for="bulk-action-selector" class="screen-reader-text">Select bulk action</label>
 			<select name="action" id="bulk-action-selector">
@@ -34,7 +29,6 @@ $table->prepare_items();
 			</select>
 			<button type="button" id="bulk-apply" class="button action">Apply</button>
 		</div>
-
 		<div class="alignright">
 			<span id="ain-status">&nbsp;</span>
 		</div>
