c9327724de8ce815d03fdab652427558ea321e28
Michael Donohoe <donohoe@gmail.com>
2025-10-14 10:32:04 -0400

    Switch from hardcoded key to one that is random and regens every 2 weeks

--- configuration.php
+++ configuration.php
@@ -4,8 +4,7 @@ if (!defined('AI_NARRATION_BASE_PATH')) {
 	define('AI_NARRATION_BASE_PATH', realpath($_SERVER['DOCUMENT_ROOT']));
 }
 
-define('AI_NARRATION_KEY',  'fskjdhg8765fs!');
-// define('AI_NARRATION_KEY',  ain_get_secret_key() );
+define('AI_NARRATION_KEY',  ain_get_secret_key() );
 
 define('AI_NARRATION_DIR',  '/wp-content/narrations');
 define('AI_NARRATION_PATH', AI_NARRATION_BASE_PATH . AI_NARRATION_DIR);
